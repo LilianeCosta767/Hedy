94b20a0362ffb0a451a1d438c33fa50f2a324256
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-09-07 22:06:14 -0300

    fazendo a primeira tela

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+    <i class="fas fa-band-aid"></i>
+  );
+}
+
+export default App;
